5b3ba7899b5933a86af907a33d6111b394be10ac
c89dev <snarvei@live.com>
2025-06-22 20:58:41 +0200

    Insert version constant only in .js files

--- sw_init.js
+++ sw_init.js
@@ -1,6 +1,6 @@
 // Initialize Service Worker
 if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('sw.js')
+      navigator.serviceWorker.register('sw.js?v=${APP_VERSION}')
         .then(() => console.log('SW registered'))
         .catch(console.error);
     }
