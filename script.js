cf0b9aba1022477f58107fa9caf0cb4ecf387563
c89dev <snarvei@live.com>
2025-06-22 19:30:50 +0200

    Attempt cache system v0.0.1

--- script.js
+++ script.js
@@ -1,12 +1,73 @@
 
     var dlcount = null;
 
-    document.getElementById('readyChoice').ontouchstart = function () {
+    // document.getElementById('readyChoice').pointerdown = function () {
+    //     const goal = document.getElementById('goal');
+    //     dlcount = Number(goal.value);
+    //     localStorage.setItem('dlcount', dlcount)
+    //     window.location.href = 'page2.html';
+    //     console.log('dlcount set to', dlcount);
+    // }
+
+    //START BUTTON
+    const handleStartClick = (e) => {
+        e.preventDefault();
         const goal = document.getElementById('goal');
         dlcount = Number(goal.value);
-        localStorage.setItem('dlcount', dlcount)
+        localStorage.setItem('dlcount', dlcount);
         window.location.href = 'page2.html';
         console.log('dlcount set to', dlcount);
     }
+
+    const startBtn = document.getElementById('readyChoice');
+    if(startBtn) {
+    ['pointerdown'].forEach(evt =>
+        startBtn.addEventListener(evt, handleStartClick)
+    );
+    }
+    
+    //DRINK BUTTON
+    const handleDrinkClick = (e) => {
+        e.preventDefault();
+        let dlcount = Number(localStorage.getItem('dlcount'));
+        if(dlcount > 0){
+            dlcount--;
+            document.getElementById('remainingLbl').textContent = 'Remaining ' + dlcount;
+            localStorage.setItem('dlcount', dlcount);
+            console.log('Remaining desiliters:', dlcount);
+            
+            if (dlcount === 0){
+                console.log('Desiliters reached!')
+            }
+        }
+        else
+        {
+            console.log('Desiliters reached!')
+            return;
+        }
+    }
+    
+    
+    const drinkBtn = document.getElementById('drinkBtnId');
+    if(drinkBtn) {
+    ['pointerdown'].forEach(evt =>
+        drinkBtn.addEventListener(evt, handleDrinkClick)
+    );
+    }
+
+    //RESET BUTTON
+    const handleResetClick = (e) => {
+        e.preventDefault();
+        dlcount = null;
+        window.location.href = 'index.html';
+    }
+
+    const resetBtn = document.getElementById('resetBtnId');
+    if(resetBtn) {
+    ['pointerdown'].forEach(evt =>
+        resetBtn.addEventListener(evt, handleResetClick)
+    );
+    }
+    
 
 
--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
-self.addEventListener('fetch', () => {});
+const CACHE_NAME = 'waterbuddy-v0.0.1'; // bump when you deploy new version
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // force update immediately
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    ).then(() => self.clients.claim()) // Take control of clients
+  );
+});
